7ea0173c6788e3d02a8f2048782729ff16a91d49
maaikexk <m.keurhorst@student.utwente.nl>
2017-12-06 12:12:07 +0100

    tijdens de les
    http://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-3-tic-tac-toe-ai-finding-optimal-move/

--- src/MinMaxPlayer.cpp
+++ src/MinMaxPlayer.cpp
@@ -14,11 +14,21 @@
 #include <vector>
 
 Position MinMaxPlayer::getNextMove(Board const& board, Field current_field) {
+	std::vector<Position> possible_moves = board.getEmptyPositions();
+	
+	assert(!possible_moves.empty());
+
+	/*for(int i=0;i<possible_moves.size;i++){
+		getMinMaxScore(board, current_field);
+	}*/
 
     return Position::unknown_position; // you need to change this in your implementation
 }
 
 int MinMaxPlayer::getMinMaxScore(Board const& board, Field current_field) {
+	int score = -1000;
+	
 
     return 0; // you need to change this in your implementation
 }
+
